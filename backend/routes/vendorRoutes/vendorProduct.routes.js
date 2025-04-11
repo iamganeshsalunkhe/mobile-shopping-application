860cbefa2038cbdf8bca4f4577b46896432c0a7f
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2025-04-11 14:00:17 +0530

    routes folder restructured for readability

---
+++ backend/routes/vendorRoutes/vendorProduct.routes.js
@@ -0,0 +1,22 @@
+// import required files
+const express = require('express');
+const vendorProductController = require("../../controllers/vendorControllers/vendorProduct.controller");
+const authenticate = require("../../middleware/authenticate");
+
+// initiate router instance 
+const router = express.Router();
+
+// get all products
+router.get('/products',authenticate,vendorProductController.getAllProducts);
+
+// add a product
+router.post('/product',authenticate,vendorProductController.newProduct);
+
+// update a product
+router.put('/product/:productId',authenticate,vendorProductController.updateProduct);
+
+// delete a product
+router.delete('/product/:productId',authenticate,vendorProductController.deleteProduct);
+
+// export router 
+module.exports = router;
