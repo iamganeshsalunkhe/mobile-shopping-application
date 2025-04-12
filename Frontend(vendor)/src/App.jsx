87e52cfa1f6606c9872fbf6b5e29285495dd3de1
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2025-04-12 19:28:20 +0530

    footer and footer's links demo component add

--- Frontend(vendor)/src/App.jsx
+++ Frontend(vendor)/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import {Toaster} from 'react-hot-toast';
 import Signup from "./components/Signup"
 import Login from "./components/Login";
+import Footer from "./components/Footer";
+import DemoComponent from "./components/DemoComponent";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route index  path="/" element={<Login/>}/>
           <Route path="signup" element={<Signup/>}/>
           <Route path="login" element={<Login/>}/>
+          <Route path="footer" element={<Footer/>}/>
+          <Route path="/demo" element={<DemoComponent/>}/>
         </Routes>
         <Toaster/>
     </BrowserRouter>
