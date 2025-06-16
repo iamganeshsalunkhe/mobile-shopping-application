41a72730654111b8664398955022f2b9680175c7
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2025-06-16 13:26:39 +0530

    react-query, react-router, toaster, react-query-devtools add

--- Frontend(customer)/src/App.jsx
+++ Frontend(customer)/src/App.jsx
@@ -1,13 +1,44 @@
+// import required modules
 import "@mantine/core/styles.css";
-
 import { MantineProvider } from "@mantine/core";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import {Toaster} from 'react-hot-toast';
+import {QueryClient,QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+
+// create queryClient instance
+const queryClient = new QueryClient();
 
 function App() {
-  return (
-    <MantineProvider>
-      
-    </MantineProvider>
+return (
+    <QueryClientProvider client = {queryClient} >
+    <BrowserRouter>
+      <MantineProvider>
+        <Routes>
+          <Route path="/" element={<SignUpPage/>} />
+        </Routes>
+      </MantineProvider>
+      <Toaster toastOptions={{
+        success:{
+          style:{
+            fontFamily:'inherit',
+            fontWeight:'bold',
+            fontSize:'18px'
+          }
+        },
+        error:{
+          style:{
+            fontFamily:"inherit",
+            fontWeight:'bold',
+            fontSize:'18px'
+          }
+        }
+      }} />
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </BrowserRouter>
+    </QueryClientProvider>
   );
 }
+
 
 export default App;
