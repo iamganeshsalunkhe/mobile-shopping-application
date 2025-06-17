d0ab8c6bdaf09ecd0ea16247026c45ea0d0063f3
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2025-06-17 11:28:05 +0530

    password visibility toggle feature add

--- Frontend(customer)/src/Components/Login.jsx
+++ Frontend(customer)/src/Components/Login.jsx
@@ -1,18 +1,27 @@
 // import required modules
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, } from "react-router-dom";
+import { FaRegEyeSlash, FaRegEye } from "react-icons/fa";
 
 function Login() {
-    return (
-      <>
-        <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 select-none bg-green-200">
-          {/* header for Login page */}
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm shadow-xl lg:min-w-xl p-4 border-1 rounded-md">
-            <h1 className="text-center font-bold text-4xl tracking-wider">
-              MSA
-            </h1>
-            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
+
+  // state for handling password visibility
+  const [isPasswordVisible, setIsPasswordVisible] = useState();
+
+  // function for handling  password visibility
+  function handlePasswordVisibility() {
+    setIsPasswordVisible((prevState) => !prevState);
+  }
+
+  return (
+    <>
+      <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 select-none bg-green-200">
+        {/* header for Login page */}
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm shadow-xl lg:min-w-xl p-4 border-1 rounded-md">
+          <h1 className="text-center font-bold text-4xl tracking-wider">MSA</h1>
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
 
           {/* input filed for email address */}
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm ">
@@ -30,7 +39,7 @@ function Login() {
                     type="email"
                     autoComplete="email"
                     className="block w-full rounded-md bg-gray-100 focus:bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 focus:scale-105"
-                    />
+                  />
                 </div>
               </div>
 
@@ -47,9 +56,17 @@ function Login() {
                   <input
                     id="password"
                     name="password"
-                    type='password'
+                    type={isPasswordVisible ? "text" : "password"}
                     className="block w-full rounded-md bg-gray-100 focus:bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 focus:scale-105"
                   />
+                  {/* handle password visibility */}
+                  <div
+                    className="absolute right-2 top-1/2 transform -translate-y-1/2
+                  cursor-pointer text-gray-600"
+                    onClick={handlePasswordVisibility}
+                  >
+                    {isPasswordVisible ? <FaRegEyeSlash /> : <FaRegEye />}
+                  </div>
                 </div>
               </div>
 
@@ -73,11 +90,10 @@ function Login() {
               </Link>
             </p>
           </div>
-          </div>
         </div>
-      </>
-    );
-
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
